303277a0f0869b5f35730d448e77f69d86d87e0b
Aaron He <aaronweihe@gmail.com>
2020-10-08 17:22:13 -0700

    Showcase ClassNotFound exception for TelephonyDisplayInfo
    In Robolectric 4.5-alpha-1

---
+++ app/src/test/java/org/aaronhe/ExampleTest.kt
@@ -0,0 +1,19 @@
+/*
+ * © 2013 - 2020 Match Group, LLC.
+ */
+package org.aaronhe
+
+import android.telephony.TelephonyManager
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ExampleTest {
+  private val telephonyManager = mock<TelephonyManager>()
+
+  @Test
+  fun example() {
+  }
+}
